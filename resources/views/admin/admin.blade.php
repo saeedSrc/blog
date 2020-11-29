87ad7eceb504d12e1558262d189d2e5730c22fb5
saeed <saeedrasooli421@gamil.com>
2020-11-30 01:05:17 +0330

    readme

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -23,7 +23,7 @@
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
 
-                <div>{{$test1}}</div>
+             admin
 
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,83 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome')->with(['var'=> 2593]);
+//});
+
+
+Route::get('/', 'App\Http\Controllers\HomeController@home');
+Route::get('/test', 'App\Http\Controllers\TestController@test');
+//Route::get('/category', 'App\Http\Controllers\CategoryController@index');
+//Route::get('/category/create', 'App\Http\Controllers\CategoryController@create');
+//Route::post('/category/store', 'App\Http\Controllers\CategoryController@store');
+//Route::get('/category/show/{id}', 'App\Http\Controllers\CategoryController@show');
+//Route::put('/category/update/{id}', 'App\Http\Controllers\CategoryController@update');
+//Route::delete('/category/destroy/{id}', 'App\Http\Controllers\CategoryController@destroy');
+
+Route::resource('category', 'App\Http\Controllers\CategoryController'); //  very good
+Route::get('user/{id}?', 'App\Http\Controllers\ShowProfile'); //  very good
+Route::get('redirect-with-helper', function () {
+    return redirect('category');
+//    return redirect()->to('category/create');
+//    return redirect()::to('category/create');
+
+} );
+
+
+
+//Route::middleware('auth')->group('/', function () {
+//    Route::get('user', function () {
+//     return view('dashboard');
+//     }
+//    );
+//
+//    Route::get('admin', function () {
+//        return view('account');
+//    }
+//    );
+//});
+
+//Route::prefix('dashboard')->group(function (){
+//    Route::get('/', function () {
+//        return view('dashboard');
+//    }
+//    );
+//
+//    Route::get('menu', function () {
+//        return view('dashboard');
+//    }
+//    );
+//});
+
+//Route::group(['prefix' => 'dashboard'],   function (){
+//    Route::get('/', function () {
+//        return 'dashboard';
+//    }
+//    );
+//
+//    Route::get('menu', function () {
+//        return 'dashboard';
+//    }
+//    );
+//});
+
+
+//Route::domain('test.mayapp')->group(function (){
+//    Route::get('/', function () {
+//        return 'dashboard';
+//    }
+//    );
+//
+//    Route::get('menu', function () {
+//        return 'dashboard';
+//    }
+//    );
+//});
+
+Route::get('redirectToCategory', function () {
+//    return redirect()->route('category.show', ['test1' => 43]);
+    return redirect()->back();
 });
+
+Route::redirect('myRoute', 'category/index');
